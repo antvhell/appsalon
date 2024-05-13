fa1284f3177176ae1893644ea6b8cfafae24f981
tOñO <antvhell@hotmail.com>
2024-05-13 16:40:31 -0600

    Autenticacion de usuarios

--- models/Usuario.php
+++ models/Usuario.php
@@ -55,6 +55,18 @@ class Usuario extends ActiveRecord {
         return self::$alertas;
     }
 
+    public function validarLogin() {
+        if(!$this->email) {
+            self::$alertas['error'][] = "El email es obligatorio";
+        }
+        if(!$this->password) {
+            self::$alertas['error'][] = "El password es obligatorio";
+        }
+
+        return self::$alertas;
+    }
+
+
     // Revisar si el usuario existe
     public function existeUsuario() {
         $query = " SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
@@ -72,6 +84,16 @@ class Usuario extends ActiveRecord {
 
     public function crearToken() {
         $this->token = uniqid();
+    }
+
+    public function comprobarPasswordAndVerificado($password) {
+        $resultado = password_verify($password, $this->password);
+        
+        if(!$resultado || !$this->confirmado){
+            self::$alertas["error"][] = "Password Incorrecto o tu cuenta no ha sido confirmada";
+        } else {
+            return true;
+        }
     }
 
 
